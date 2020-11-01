797a5653ba2d88e6d7fa9d54fce01565cc33a67c
lgrayes <lgrayes@gmail.com>
2020-11-01 16:30:18 -0600

    update keys

--- starter_code/WeatherPy/api_keys.py
+++ starter_code/WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b9319c8e93ac9db2c291e5a9b15aff5f"
+weather_api_key = "text"
 
 # Google API Key
-g_key = "AIzaSyCZfMcN_xj9dMPJl8Fhbq6rOP9KlnF0llw"
+g_key = "text"
